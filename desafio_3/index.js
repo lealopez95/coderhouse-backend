3bb065d02ec97b7244689f9de187229672437877
Leandro Lopez <leandroe.lopez95@gmail.com>
2022-08-29 23:22:35 -0300

    fix folder structure and challenge 3

--- desafio_3/index.js
+++ desafio_3/index.js
@@ -1,10 +1,36 @@
-const http = require('http');
+const Container = require('../classes/Container');
+const { randomIntFromInterval } = require('../helpers/utils');
 
-//create server
-const server = http.createServer((request, response) => {
-    response.write('hola mundo');
-    response.end();
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+const container = new Container('products');
+
+app.get('/', (request, response) => {
+    response.send('Pagina de inicio')
 });
 
-//listen
-server.listen(8080);
+app.get('/productos', async (request, response) => {
+    response.send(await container.getAll());
+});
+
+app.get('/productosRandom', async (request, response) => {
+    const randomId = randomIntFromInterval(1, 3);
+    response.send(await container.getById(randomId));
+});
+
+
+app.get('*', (request, response) => { // cae aca si no poasa ninguna regla de arriba
+    response.send('<h1>NOT FOUND</h1>')
+});
+
+const connectedServer = app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`)
+});
+
+connectedServer.on('error', error => {
+    console.log(error);
+});
